e2f97e353131409142ac343a61351e00e6e81aa1
ishita37381 <142157899+ishita37381@users.noreply.github.com>
2024-07-15 11:33:20 +0530

    Update diameterOfBinaryTree.cpp

--- day 1/diameterOfBinaryTree.cpp
+++ day 1/diameterOfBinaryTree.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+int height(int&ans,TreeNode* root){
+    if(!root)return 0;
+    int left=height(ans,root->left);
+    int right=height(ans,root->right);
+    ans=max(ans,left+right);
+    return max(left,right)+1;
+}
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root)return 0;
+        int ans=0;
+        height(ans,root);
+        return ans;
+    }
+};
