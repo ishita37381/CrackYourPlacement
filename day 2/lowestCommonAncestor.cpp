df2e2f857a056c3f3605fa755061010f647dabec
ishita37381 <142157899+ishita37381@users.noreply.github.com>
2024-07-16 13:20:57 +0530

    Create lowestCommonAncestor.cpp

---
+++ day 2/lowestCommonAncestor.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL)return NULL;
+        if(root==p or root==q)return root;
+        TreeNode * left=lowestCommonAncestor(root->left,p,q);
+        TreeNode * right=lowestCommonAncestor(root->right,p,q);
+        if(left!=nullptr and right!=NULL)return root;
+        if(left)return left;
+        else return right;
+
+    }
+};
