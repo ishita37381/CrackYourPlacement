9f71f91c7965b8ee6caf1b9408cb7f6d462abb54
ishita37381 <142157899+ishita37381@users.noreply.github.com>
2024-07-17 17:31:24 +0530

    Create linkedListCycle.cpp

---
+++ day 3/linkedListCycle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* slow=head;
+        ListNode*fast=head;
+        while(fast and fast->next){
+           fast=fast->next->next;
+           slow=slow->next;
+           if(fast==slow)return true;
+        }
+        return false;
+    }
+};
