6b84b5d7a6b1c5494c8c118cccde452465e387f8
ishita37381 <142157899+ishita37381@users.noreply.github.com>
2024-07-19 12:50:55 +0530

    Create validateBST.cpp

---
+++ day 5/validateBST.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+vector<int>nodes;
+void inorder(TreeNode* root){
+    if(root->left)inorder(root->left);
+    nodes.push_back(root->val);
+    if(root->right)inorder(root->right);
+}
+    bool isValidBST(TreeNode* root) {
+        inorder(root);
+        for(int i=0;i<nodes.size()-1;i++){
+            if(nodes[i]>=nodes[i+1])return false;
+        }
+        return true;
+        
+    }
+};
