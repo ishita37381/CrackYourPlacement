b47924bd3498974f73a905d066ba2a0f7370ac54
ishita37381 <142157899+ishita37381@users.noreply.github.com>
2024-07-22 18:12:00 +0530

    Create generate parentheses.cpp

---
+++ day 8/generate parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+void solve(int total,int open,int close,string s,vector<string>&ans){
+    if(s.size()==total){
+        ans.push_back(s);
+        return;
+    }
+    if(open>close){
+        solve(total,open,close+1,s+')',ans);
+        if(open<total/2){
+            solve(total,open+1,close,s+'(',ans);
+ }}
+    else{
+        solve(total,open+1,close,s+'(',ans);
+    }
+}
+    vector<string> generateParenthesis(int n) {
+        int total=2*n;
+        vector<string>ans;
+        solve(total,0,0,"",ans);
+        return ans;
+    }
+};
