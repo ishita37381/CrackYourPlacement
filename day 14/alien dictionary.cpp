6837bb9ef9f9b96f0e2e33c3c5f8d68f6c4e56d5
ishita37381 <142157899+ishita37381@users.noreply.github.com>
2024-07-28 11:36:47 +0530

    Create alien dictionary.cpp

---
+++ day 14/alien dictionary.cpp
@@ -0,0 +1,58 @@
+// User function Template for C++
+
+class Solution{
+    public:
+    vector<int> toposort(int K, vector<int>adj[])
+    {
+        int indegree[K]={0};
+        for(int i = 0; i < K; i++)
+        {
+            for(auto it:adj[i])
+                indegree[it]++;
+        }
+        
+        queue<int>q;
+        for(int i = 0; i < K; i++)
+        {
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        
+        vector<int>topo;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            
+            for(auto it:adj[node])
+            {
+                indegree[it]--;
+                if(indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+        return topo;
+    }
+    string findOrder(string dict[], int N, int K) {
+        //code here
+        vector<int>adj[K];
+        for(int i=0;i<N-1;i++){
+            string s1=dict[i];
+            string s2=dict[i+1];
+            int len=min(s1.size(),s2.size());
+            for(int ptr=0;ptr<len;ptr++){
+                if(s1[ptr]!=s2[ptr]){
+                    adj[s1[ptr]-'a'].push_back(s2[ptr]-'a');
+                    break;
+                }
+            }
+        }
+        vector<int>topo=toposort(K,adj);
+        string ans="";
+        for(auto it:topo){
+            ans=ans+char(it+'a');
+        }
+        return ans;
+    }
+};
