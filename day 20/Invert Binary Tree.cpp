a587264a267c9d2a1ff7b65c6ecc0a68dbf970b2
ishita37381 <142157899+ishita37381@users.noreply.github.com>
2024-08-03 18:58:23 +0530

    Create Invert Binary Tree.cpp

---
+++ day 20/Invert Binary Tree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==nullptr)return NULL;
+        invertTree(root->left);
+        invertTree(root->right);
+        TreeNode* temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+        return root;
+    }
+};
