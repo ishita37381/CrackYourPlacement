ae584dbae8943e2921ecc11086e3b2c86d031173
ishita37381 <142157899+ishita37381@users.noreply.github.com>
2024-08-08 19:31:28 +0530

    Create Subtree of Another Tree.cpp

---
+++ day 25/Subtree of Another Tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+bool isidentical(TreeNode* a, TreeNode* b){
+        if(a==NULL && b==NULL)
+            return true;
+        if(a==NULL || b==NULL)
+            return false;
+        if(a->val!=b->val)
+            return false;
+        return isidentical(a->left,b->left) && isidentical(a->right,b->right);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(subRoot==NULL)
+            return true;
+        if(root==NULL)
+            return false;
+        if(isidentical(root,subRoot))
+            return true;
+        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
+    }
+};
