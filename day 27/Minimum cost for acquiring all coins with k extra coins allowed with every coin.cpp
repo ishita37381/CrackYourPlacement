cbb6810ecc401eb8297c63d079f06429c694cf6f
ishita37381 <142157899+ishita37381@users.noreply.github.com>
2024-08-10 19:07:32 +0530

    Create Minimum cost for acquiring all coins with k extra coins allowed with every coin.cpp

---
+++ day 27/Minimum cost for acquiring all coins with k extra coins allowed with every coin.cpp
@@ -0,0 +1,12 @@
+int minCost(int coin[], int n, int k)  
+{ 
+    sort(coin, coin + n); 
+ 
+    int coins_needed = ceil(1.0 * n /  
+                            (k + 1));
+    int ans = 0; 
+    for (int i = 0; i <= coins_needed - 1;i++)  
+        ans += coin[i]; 
+      
+    return ans; 
+} 
