b106c3bea48e686e3d0c75b474893e54a419e581
ishita37381 <142157899+ishita37381@users.noreply.github.com>
2024-08-11 18:22:34 +0530

    Create Merge k Sorted Lists.cpp

---
+++ day 28/Merge k Sorted Lists.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) { 
+        if(l1 == NULL)
+            return l2;
+        if(l2 == NULL)
+            return l1;
+
+        ListNode* result;
+        if(l1->val <= l2->val){
+            result = l1;
+            result->next = mergeTwoLists(l1->next, l2);
+        }else{
+            result = l2;
+            result->next = mergeTwoLists(l1, l2->next);
+        }
+
+        return result;
+    }
+
+    ListNode* partitionAndMerge(int start, int end, vector<ListNode*>& lists){
+        if(start > end)
+            return NULL;
+        if(start == end){  
+            return lists[start];
+        }
+
+        int mid = (start + end) / 2;
+
+        ListNode* left = partitionAndMerge(start, mid, lists);
+        ListNode* right = partitionAndMerge(mid + 1, end, lists);
+
+        return mergeTwoLists(left, right);
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k = lists.size();
+        if(k == 0)  return NULL;
+        int start = 0;
+        int end = k - 1;
+
+        return partitionAndMerge(start, end, lists);
+    }
+};
