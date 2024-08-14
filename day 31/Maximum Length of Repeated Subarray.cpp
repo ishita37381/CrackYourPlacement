a81ec805637adb12c93ee74826c197eee3ce1abb
ishita37381 <142157899+ishita37381@users.noreply.github.com>
2024-08-14 18:40:13 +0530

    Create Maximum Length of Repeated Subarray.cpp

---
+++ day 31/Maximum Length of Repeated Subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int maxi=0;
+        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+        for(int i=0;i<=n1;i++){
+            for(int j=0;j<=n2;j++){
+                if(i==0 or j==0)dp[i][j]=0;
+                else{
+                    if(nums1[i-1]==nums2[j-1])dp[i][j]=1+dp[i-1][j-1];
+                    else dp[i][j]=0;
+                }
+                maxi=max(maxi,dp[i][j]);
+            }
+        }
+        return maxi;
+    }
+};
